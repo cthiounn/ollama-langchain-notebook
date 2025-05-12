152bd2dff00c6cfcb6f8ff92220206837899392d
cthiounn2 <thiounn.conrad@gmail.com>
2025-05-12 09:02:31 +0000

    don't load cach

--- load_run_ollama.sh
+++ load_run_ollama.sh
@@ -1,8 +1,1 @@
-if test -d "$HOME/.ollama"
-then
-    echo "~/.ollama directory exists"
-else
-    echo "~/.ollama directory does not exist"
-    cd ~ && mc cp -r s3/$VAULT_TOP_DIR/Caches/.ollama .
-fi
 ollama serve &
